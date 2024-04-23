a98769d2d8dbb76c349bba6cc33eb7d98b24a718
Khaled Masum <khaledmasum1@gmail.com>
2024-04-23 22:04:53 +0600

    Challenging part completed

--- utilities.js
+++ utilities.js
@@ -1,16 +1,8 @@
-function decreaseRemainingSeat(elementId) {
+function defaultSeat(elementId) {
     const currentSeatId = document.getElementById(elementId);
     const currentSeatText = currentSeatId.innerText;
     const currentSeat = parseInt(currentSeatText);
     const availableSeat = currentSeat - 1;
-    currentSeatId.innerText = availableSeat;
-}
-
-function increaseRemainingSeat(elementId) {
-    const currentSeatId = document.getElementById(elementId);
-    const currentSeatText = currentSeatId.innerText;
-    const currentSeat = parseInt(currentSeatText);
-    const availableSeat = currentSeat + 1;
     currentSeatId.innerText = availableSeat;
 }
 
@@ -18,16 +10,11 @@ function increaseCurrentSeat(elementId) {
     const currentSeatId = document.getElementById(elementId);
     const currentSeatText = currentSeatId.innerText;
     const currentSeat = parseInt(currentSeatText);
-    const availableSeat = currentSeat + 1;
-    currentSeatId.innerText = availableSeat;
-}
-
-function decreaseCurrentSeat(elementId) {
-    const currentSeatId = document.getElementById(elementId);
-    const currentSeatText = currentSeatId.innerText;
-    const currentSeat = parseInt(currentSeatText);
-    const availableSeat = currentSeat - 1;
-    currentSeatId.innerText = availableSeat;
+    const selectedSeat = currentSeat + 1;
+    if (currentSeat == 4) {
+        document.removeEventListener('click', selectSeat);
+    }
+    currentSeatId.innerText = selectedSeat;
 }
 
 function getSeatNumber(event) {
@@ -55,22 +42,19 @@ function appendNewRow(elementId) {
 
 function selectSeat(event) {
     const seatElement = event.target;
+    const currentSeatId = document.getElementById('selected-seat').innerText;
+    const currentSeat = parseInt(currentSeatId);
 
-    if (seatElement.classList.contains('bg-green-400')) {
-        seatElement.classList.remove('bg-green-400');
-        decreaseCurrentSeat('selected-seat');
-        increaseRemainingSeat('current-seat');
-
-    } else {
+    if (currentSeat <= 3 && !seatElement.classList.contains('bg-green-400')) {
         seatElement.classList.add('bg-green-400');
+        defaultSeat('default-seat');
         increaseCurrentSeat('selected-seat');
-        decreaseRemainingSeat('current-seat');
         appendNewRow('table-content');
+        getSeatNumber(event);
+        calculateTotalPrice();
+        enableNextButton();
+        activeCoupon('selected-seat');
     }
-
-    getSeatNumber(event);
-    calculateTotalPrice();
-    enableNextButton();
 }
 
 function calculateTotalPrice() {
@@ -104,8 +88,6 @@ function enableNextButton() {
     const mobileNumberText = document.getElementById('mobile-number').value;
     const mobileNumber = parseInt(mobileNumberText);
 
-
-
     const selectedSeat = document.getElementById('selected-seat');
     const selectedSeatText = selectedSeat.innerText;
     const selectedSeatNumber = parseInt(selectedSeatText);
@@ -116,5 +98,72 @@ function enableNextButton() {
         buttonId.disabled = true;
     }
 }
+
+function activeCoupon(elementId) {
+    const currentSeatId = document.getElementById(elementId).innerText;
+    const currentSeat = parseInt(currentSeatId);
+
+    if (currentSeat == 4) {
+        document.getElementById('apply-for-coupon').disabled = false;
+        document.getElementById('coupon-input').disabled = false;
+    }
+}
+
+function createDiscountPriceRow() {
+    const tableFooter = document.getElementById('table-footer');
+    const tableRow = document.createElement('tr');
+    tableRow.innerHTML = `
+                <td colspan="2" class="font-semibold font-inter text-black">Discount Price</td>
+                <td class="font-semibold font-inter text-black">BDT <span id="discount-price">0</span></td>
+            `
+    tableFooter.appendChild(tableRow);
+}
+
+function applyCoupon() {
+    const new15Coupon = document.getElementById('new15-coupon').innerText;
+    const couple20Coupon = document.getElementById('couple20-coupon').innerText;
+    let userTypedCouponCode = document.getElementById('coupon-input').value;
+
+    if (new15Coupon === userTypedCouponCode || couple20Coupon === userTypedCouponCode) {
+        let currentTotal = document.getElementById('current-total');
+        let currentTotalText = currentTotal.innerText;
+        let currentTotalNumber = parseInt(currentTotalText);
+
+        let grandTotal = document.getElementById('grand-total');
+        let grandTotalText = grandTotal.innerText;
+        let grandTotalNumber = parseInt(grandTotalText);
+
+        if (new15Coupon === userTypedCouponCode) {
+            const discountPrice = (currentTotalNumber / 100) * 15;
+            const reducedGrandTotal = grandTotalNumber - discountPrice;
+            grandTotal.innerText = reducedGrandTotal;
+
+            createDiscountPriceRow();
+
+            const discountPriceId = document.getElementById('discount-price');
+            discountPriceId.innerText = discountPrice;
+
+            const couponContainer = document.getElementById('coupon-container');
+            couponContainer.classList.add('hidden');
+        } else if (couple20Coupon === userTypedCouponCode) {
+            const discountPrice = (currentTotalNumber / 100) * 20;
+            const reducedGrandTotal = grandTotalNumber - discountPrice;
+            grandTotal.innerText = reducedGrandTotal;
+
+            createDiscountPriceRow();
+
+            const discountPriceId = document.getElementById('discount-price');
+            discountPriceId.innerText = discountPrice;
+
+            const couponContainer = document.getElementById('coupon-container');
+            couponContainer.classList.add('hidden');
+        }
+    } else {
+        alert('Invalid Coupon');
+        document.getElementById('coupon-input').value = '';
+    }
+}
+
+
 
 
